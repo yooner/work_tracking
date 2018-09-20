dbce03c51525ccbfbfb5361a14885fb7afe9ee6d
miao.shijun <411890573@qq.com>
2018-09-20 19:24:34 +0800

    first commit

---
+++ work_tracking.js
@@ -0,0 +1,37 @@
+new Vue({
+    el: '#example',
+    data:{
+        goal_array:[
+            {
+                obj:'有钱',
+                estimate_tomato:10,
+                cost_tomato:10
+            }
+        ],
+        mygoal:'',
+        mytomato:0,
+        checkedGoals:[],
+        tempgoals:[],
+        checkedfeedback:[],
+        feedback_goals:[],
+        reasonarray:[],
+        selectedreason:''
+    },
+    methods:{
+        add: function () {
+            var goal_object = {
+                obj:'',
+                estimate_tomato:0,
+                cost_tomato:0
+            };
+            goal_object.obj = this.mygoal;
+            goal_object.estimate_tomato = this.mytomato;
+            this.goal_array.push(goal_object);
+            console.log(this.goal_array);
+        },
+        check: function () {
+            //
+        }
+
+    }
+});
